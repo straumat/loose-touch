25c26f68425626298cbc18f7859246ddd2f111b3
straumat <stephane.traumat@gmail.com>
2019-03-31 11:10:33 +0200

    Fix lint errors

--- clients/web/application/src/app/core/core.module.ts
+++ clients/web/application/src/app/core/core.module.ts
@@ -4,7 +4,7 @@ import {CoreComponent} from './core.component';
 import {HeaderComponent} from './header/header.component';
 import {AppRoutingModule} from '../app-routing.module';
 import {CoreRoutingModule} from './core-routing.module';
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [CoreComponent, HeaderComponent],
--- clients/web/application/src/app/features/features.module.ts
+++ clients/web/application/src/app/features/features.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {DummyComponent} from './dummy/dummy.component';
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [DashboardComponent, DummyComponent],
